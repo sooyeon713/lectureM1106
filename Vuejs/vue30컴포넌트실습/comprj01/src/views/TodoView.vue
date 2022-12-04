3a49efef85252363c93ceae890357a54850eece8
sooyeon713 <eldkfdms@gmail.com>
2022-12-04 09:53:42 +0900

    vue끝

--- Vuejs/vue30컴포넌트실습/comprj01/src/views/TodoView.vue
+++ Vuejs/vue30컴포넌트실습/comprj01/src/views/TodoView.vue
@@ -28,8 +28,7 @@ button {
       v-bind:todoItems="todoItems"
       v-on:doneToggle="doneToggle"
       v-on:removeTodo="removeTodo"
-    >
-    </TodoList>
+    ></TodoList>
 
     <!-- TodoFooter -->
     <TodoFooter v-on:clearAll="clearAll"></TodoFooter>
@@ -48,92 +47,86 @@ export default {
   props: [],
   data() {
     /* 컴포넌트 안에서 사용되는 변수 등록. 개별 변수 */
-    /* data 프로퍼티 값 변경시 this.set(object, key, value) 을 사용 */
     return {
-      /*todoItems: [
-        { id: 1, todo: '영화보기', done: false },
-        { id: 2, todo: '주말 산책', done: true },
-        { id: 3, todo: 'ES6 학습', done: false },
-        { id: 4, todo: '잠실 야구장', done: false },
-      ],*/
+      // todoItems: [
+      //   { id: 1, todo: '영화보기', done: false },
+      //   { id: 2, todo: '주말 산책', done: true },
+      //   { id: 3, todo: 'ES6 학습', done: false },
+      //   { id: 4, todo: '잠실 야구장', done: false },
+      // ],
     };
   },
   //template: ``,
   methods: {
     /* 이벤트 핸들러 등록 + 일반 함수 */
-    clearAll(e) {
-      debugger;
-      console.log(e.target);
-      //this.$data.todoItems = []; // 빈배열을 넣음으로써 삭제
+    clearAll() {
+      // this.$data.todoItems = [];
+      this.dispatchClearAll();
+    },
+    doneToggle(id) {
+      // 불변객체
+      // ==> 복제 후 재할당 방식으로 처리
+      // ==> 1. map, filter, reduce
+      //     2. spread 연산자: ...
+      //     3. 라이브러리 방식: immer, immutable
+      // const newTodos = this.$data.todoItems.map((item /* , index, array */) => {
+      //   if (item.id === id) {
+      //     item.done = !item.done;
+      //   }
+      //   return item;
+      // }); // 복제
+      // this.$data.todoItems = newTodos; // 재할당
+      this.dispatchDoneToggle(id);
+    },
+    removeTodo(id) {
+      // 불변객체
+      // ==> 복제 후 재할당 방식으로 처리
+      // ==> 1. map, filter, reduce
+      //     2. spread 연산자: ...
+      //     3. 라이브러리 방식: immer, immutable
+      // const newTodos = this.$data.todoItems.filter((item) => {
+      //   if (item.id === id) {
+      //     return false;
+      //   }
+      //   return true;
+      // }); // 복제
+      // this.$data.todoItems = newTodos; // 재할당
+      this.dispatchRemoveTodo(id);
     },
     addTodo(e, newTodoItem) {
-      debugger;
-      console.log(e.target);
-      console.log(newTodoItem);
-
-      // newTodoItem 값이 없으면 종료한다. 빈값 호출 방지.
+      // debugger;
+      // console.log(e.target);
+      // console.log(newTodoItem);
+      // // newTodoItem 값이 없으면 종료한다. 빈값 호출 방지.
       // if (!newTodoItem) {
       //   return;
       // }
-
-      // max id 구하기 ==> map과 reduce 를 사용하여
-      //    1. id 값 만 있는 새로운 배열을 만든다. ==> map() 메서드 사용.
-      //    2. map() 메서드로 만들어진 새로운 배열에서 최대값을 찾는다. ===> reduce() 메서드 사용
-      //    3. 추가될 새로운 id = max id + 1
+      // // max id 구하기 ==> map과 reduce 를 사용하여
+      // //    1. id 값 만 있는 새로운 배열을 만든다. ==> map() 메서드 사용.
+      // //    2. map() 메서드로 만들어진 새로운 배열에서 최대값을 찾는다. ===> reduce() 메서드 사용
+      // //    3. 추가될 새로운 id = max id + 1
       // const ids = this.$data.todoItems.map((item) => item.id);
       // console.log(ids);
-      // // 하면 안됌 maxid = ids[ids.length - 1] + 1;
+      // // maxid = ids[ids.length - 1] + 1;
       // const maxid = ids.reduce((pvalue, cvalue) => {
       //   if (pvalue > cvalue) return pvalue;
       //   else return cvalue;
       // }, 0);
       // console.log(maxid);
-
       // const newid = maxid + 1;
-
       // // todoItems 추가할 객체 만들기
       // const newTodo = {
       //   id: newid,
       //   todo: newTodoItem,
       //   done: false,
       // };
-
-      // // todoItems 배열에 객체 추가, 복제후 재할당 방식 사용하여
-      // //this.$data.todoItems.push(newTodo);
+      // // todoItems 배열에 객체 추가. 복제후재할당 방식 사용하여
+      // // this.$data.todoItems.push(newTodo);
       // this.$data.todoItems = [...this.$data.todoItems, newTodo];
-      dispatchAddTodo(newTodoItem);
-    },
-    doneToggle(id) {
-      debugger;
-      console.log(id);
-      // 불변객체
-      // ==> 복제 후 재할당 방식으로 처리
-      // ==> 1. map, filter, reduce 사용  // 선택해서 바꿀  경우 많이  사용 됨
-      //     2. spread 연산자 사용
-      //     3. 라이브러리  방식 사용 : immer, immutable
-      // const newTodos = this.$data.todoItems.map((item /*, index, array*/) => {
-      //   if (item.id === id) {
-      //     item.done = !item.done;
-      //   }
-      //   return item;
-      // }); //복제
-      // this.$data.todoItems = newTodos; //재할당
-      dispatchDoneToggle(id);
-    },
-    removeTodo(id) {
-      // debugger;
-      // console.log(id);
-      // const newTodos = this.$data.todoItems.filter((item) => {
-      //   if (item.id === id) {
-      //     return false;
-      //   }
-      //   return true;
-      // }); //복제
-      // this.$data.todoItems = newTodos; //재할당
-      dispatchRemoveTodo(id);
+      this.dispatchAddTodo(newTodoItem);
     },
     /* vuex 를 사용하는 경우
-      mapActions 는 store의 actions 를 가져옵니다.
+      mapActions 는 store의 actions 를 가져오는 헬퍼 메서드니다.
       namespaced: true를 설정한 경우 네임스페이스를 사용하기 때문에 store의 모듈 명을 적어주어야 합니다.
       store 모듈에서 actions 를 가져오는 2가지 방식
       1) store.모듈명.actions 이름 바꾸어 사용하기(추천방식)
@@ -141,7 +134,7 @@ export default {
       2) store.모듈명.actions 이름 그대로 사용하기
          ...mapActions('모듈명', ['액션명1', '액션명2']),
       */
-    ...mapActions('storeTodo', {
+    ...mapActions('todoStore', {
       dispatchClearAll: 'clearAll',
       dispatchDoneToggle: 'doneToggle',
       dispatchRemoveTodo: 'removeTodo',
@@ -160,7 +153,7 @@ export default {
   computed: {
     /* 자동처리 + 동기식. 메서드로 작성. return 필수. data 와 공존 불가 */
     /* vuex 를 사용하는 경우
-      mapGetters 는 store의 getters 를 가져옵니다.
+      mapGetters 는 store의 getters 를 가져오는 헬퍼메서드 입니다.
       namespaced: true를 설정한 경우 네임스페이스를 사용하기 때문에 store의 모듈 명을 적어주어야 합니다.
       store 모듈에서 getters 를 가져오는 2가지 방식
       1) store.모듈명.getters 이름 바꾸어 사용하기
